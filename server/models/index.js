abbe935a29dc1f48439622b81c6372ec1970e381
stop-zero <jungjy0097@naver.com>
2023-04-25 03:22:47 +0900

    setting

---
+++ server/models/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const config = require(__dirname + '/../config/config.json')['development'];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+// TODO: model 연결
+
+module.exports = db;
